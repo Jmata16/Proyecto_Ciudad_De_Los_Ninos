e94ea28e1d22cb6032645f337d2f71b5031b72b4
DYL4N1616 <dyurenapiedra@gmail.com>
2024-04-08 00:56:09 -0600

    Modulo de inventario parte visual

---
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/Controllers/AdministracionDeInventarioController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Ciudad_De_Los_Ninos.Controllers
+{
+    public class AdministracionDeInventarioController : Controller
+    {
+        public IActionResult Alimentos()
+        {
+            return View();
+        }
+
+        public IActionResult HigienePersonal()
+        {
+            return View();
+        }
+
+        public IActionResult AgregarProducto()
+        {
+            return View();
+        }
+
+        public IActionResult GuardarProducto()
+        {
+            return View();
+        }
+        public IActionResult EliminarProducto()
+        {
+            return View();
+        }
+        public IActionResult EditarProducto(int id)
+        {
+            var producto = (id);
+            return View(producto);
+        }
+    }
+}
