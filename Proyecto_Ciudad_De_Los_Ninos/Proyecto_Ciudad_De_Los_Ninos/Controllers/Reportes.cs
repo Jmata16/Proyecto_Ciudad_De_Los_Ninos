fdbfb7fc597a44c0dafe819db807787458fe9d06
Daniela Ceciliá <danielaceciliajcr@gmail.com>
2024-04-09 00:27:31 -0600

    Módulo Reportes: Citas, Incidentes, Pruebas de Dopaje, Reportes Médicos

---
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/Controllers/Reportes.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Ciudad_De_Los_Ninos.Controllers
+{
+    public class Reportes : Controller
+    {
+        public IActionResult Citas()
+        {
+            return View();
+        }
+        public IActionResult Incidentes()
+        {
+            return View();
+        }
+        public IActionResult PruebasDopaje()
+        {
+            return View();
+        }
+        public IActionResult ReportesMedicos()
+        {
+            return View();
+        }
+    }
+}
