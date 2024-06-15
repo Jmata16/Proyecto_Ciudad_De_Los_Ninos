84f7e3250eeafd508e39fd8ae00e3a9297888684
Karen J Román <84334642+UpsKaren@users.noreply.github.com>
2024-06-14 18:09:01 -0600

    Arreglo del modelo Inventario
    Validaciones,Crud y nombres de los Cruds

--- Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/Models/Inventario_Higiene_Personal.cs
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/Models/Inventario_Higiene_Personal.cs
@@ -9,9 +9,25 @@ namespace API_Ciudad_De_Los_Ninos.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre del producto no puede exceder los 50 caracteres.")]
         public string nombre_producto { get; set; }
+
+
+        [Required(ErrorMessage = "La candidad disponible  es obligatoria.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad disponible debe ser un número positivo.")]
         public int cantidad_disponible { get; set; }
+
+
+        [Required(ErrorMessage = "La fecha de la última reposición es obligatoria.")]
+        [DataType(DataType.Date, ErrorMessage = "Formato de fecha no  es válido.")]
+        [Range(typeof(DateTime), "01/01/1900", "01/01/2100", ErrorMessage = "La fecha de última reposición debe estar entre el 01/01/2000 y el 01/01/2100.")]
         public DateTime fecha_ultima_reposicion { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre del proveedor es obligatorio.")]
+        [StringLength(60, ErrorMessage = "El nombre del proveedor no puede exceder los 60 caracteres.")]
         public string proveedor { get; set; }
     }
 }
--- Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396ab77e610e2cdc85151cbbef8192f681861802")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d6ec9fa816996a895872d683bfe22ba943ce9d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
