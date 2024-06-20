8ef39f2c4c244c0c4e984dc68df3c5e2da57d11a
Karen J Román <84334642+UpsKaren@users.noreply.github.com>
2024-06-19 19:51:15 -0600

    Creación de la tabla Tickete

---
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/Models/Tickete.cs
@@ -0,0 +1,29 @@
+﻿using API_Ciudad_De_Los_Ninos.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto_Ciudad_De_Los_Ninos.Models
+{
+    public class Tickete
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo joven es requerido")]
+        public int id_joven { get; set; }
+
+        [Required(ErrorMessage = "El campo Higiene personal es requerido para el producto")]
+        public int id_inventario_higiene_personal { get; set; }
+
+        [Required(ErrorMessage = "El campo Tickete es requerido")]
+        public int tickete { get; set; }
+
+        public Inventario_Higiene_Personal inventario_Higiene_Personal { get; set; }
+
+        public Jovenes? Joven { get; set; }
+      
+    
+    }
+}
--- Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f7e3250eeafd508e39fd8ae00e3a9297888684")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0296ccd548fad06128b91c0f3aee65d0f733dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
