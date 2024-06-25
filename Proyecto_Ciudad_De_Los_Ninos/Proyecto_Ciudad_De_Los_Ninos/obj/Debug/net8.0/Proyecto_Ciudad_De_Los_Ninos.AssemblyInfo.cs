5b177427a9781b14e7aaadffa4ba2e92edc7c331
Daniela Ceciliá <danielaceciliajcr@gmail.com>
2024-06-24 23:09:52 -0600

    Diseño CRUD Pruebas Dopaje
    Diseño CRUD Pruebas Dopaje

--- Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587b7cb014df401077d06c0d2a54a1c2195a87e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88160f5912619184ee85339e7e7e19799e30079c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
