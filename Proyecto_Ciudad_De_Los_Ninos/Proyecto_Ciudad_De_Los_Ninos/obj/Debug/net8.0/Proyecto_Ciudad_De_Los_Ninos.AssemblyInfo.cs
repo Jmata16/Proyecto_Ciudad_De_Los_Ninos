4622d4f092aa5de0d2684f845f1b56239dfc46a8
Karen J Román <84334642+UpsKaren@users.noreply.github.com>
2024-07-01 15:24:37 -0600

    Arreglo footer admi de empleados
    Co-Authored-By: Jose Daniel Mata <105888177+Jmata16@users.noreply.github.com>

--- Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+577e1e20657e4bc8af86ab3660d7db9e0623673d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87fbd47238cf7e68c9f8910b138376b6d831d78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
