1c7f831866719ad7c898c9daa380d3cb86e75ee3
Daniela Ceciliá <danielaceciliajcr@gmail.com>
2024-07-02 01:41:36 -0600

    Creación Gráfico Pruebas Dopaje

---
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/Controllers/ChartsDopajeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Ciudad_De_Los_Ninos.Models;
+
+namespace Proyecto_Ciudad_De_Los_Ninos.Controllers
+{
+    public class ChartsDopajeController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ChartsDopajeController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var resultados = _context.Pruebas_Dopaje
+                .GroupBy(p => p.resultado)
+                .Select(g => new { Resultado = g.Key, Count = g.Count() })
+                .ToList();
+
+            return View(resultados);
+        }
+    }
+}
--- Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c191f95c5198c1742404ba821e07f68fd7838e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1b9b2a5cd632558ce9b44045e706eb97300934")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
