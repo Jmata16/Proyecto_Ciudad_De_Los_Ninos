54eccc5fabfab58d59954e16ca394d39110df5d5
Daniela Ceciliá <danielaceciliajcr@gmail.com>
2024-07-09 10:33:03 -0600

    Cambios Diseño Reportes Expedientes

--- Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+121a3e08a6baf842320fe0f52705e0bc12ff379d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d054c6a20b572419b2fbaa13098e3da421ad52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
