fce775df7011ba970b8ea33d0a4e0c6b84393c46
Jose Daniel Mata <jdmata16@gmail.com>
2024-07-15 19:50:27 -0600

    Squashed commit of the following:
    commit 09fa8d8082423f4b015347d622117893b3890c3b
    Author: Daniela Ceciliá <danielaceciliajcr@gmail.com>
    Date:   Mon Jul 15 15:13:21 2024 -0600
    
        Primera Parte Creación Mapa
    
    commit 753b7066981fd37127d6f41fe031a9f6f3b57a56
    Author: Jose Daniel Mata <jdmata16@gmail.com>
    Date:   Mon Jul 1 12:35:55 2024 -0600
    
        Revert "Se agregan columnas de Resultado y Observaciones, Cambios en diseño"
    
        This reverts commit 6304b55b2840299d8c269d04fce5426450e9adee.
    
    commit 6304b55b2840299d8c269d04fce5426450e9adee
    Author: Daniela Ceciliá <danielaceciliajcr@gmail.com>
    Date:   Mon Jul 1 11:03:46 2024 -0600
    
        Se agregan columnas de Resultado y Observaciones, Cambios en diseño

---
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/Controllers/MapaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Ciudad_De_Los_Ninos.Controllers
+{
+    public class MapaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
