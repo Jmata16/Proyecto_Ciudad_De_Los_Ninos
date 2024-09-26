8d4590bdfce604d722132651930a59ecc55f59f2
Jose Daniel Mata <jdmata16@gmail.com>
2024-09-25 21:10:25 -0600

    cambios

--- Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
+++ Proyecto_Ciudad_De_Los_Ninos/Proyecto_Ciudad_De_Los_Ninos/obj/Debug/net8.0/Proyecto_Ciudad_De_Los_Ninos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1289450ff175f4e9a870798a743a5279f771057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26298f0884f899c46ed010d7234217a71aab13e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Ciudad_De_Los_Ninos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
